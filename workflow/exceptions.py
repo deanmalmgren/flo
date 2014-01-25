cc3c1e37ae762d9cd6b82571009eb931147a109a
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-01-25 05:59:50 -0600

    catch ConfigurationNotFound from command line

--- workflow/exceptions.py
+++ workflow/exceptions.py
@@ -1,10 +1,15 @@
-class ConfigurationNotFound(Exception):
+# convenience exception to make it easy to not print traceback when
+# run from the command line
+class CommandLineException(Exception):
+    pass
+
+class ConfigurationNotFound(CommandLineException):
     def __init__(self, config_filename, cwd):
         self.config_filename = config_filename
         self.cwd = cwd
 
     def __str__(self):
-        return "\nNo configuration file called '%s' found in directory\n'%s'"%(
+        return "No configuration file called '%s' found in directory\n'%s'"%(
             self.config_filename, 
             self.cwd,
         )
