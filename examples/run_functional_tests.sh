b51f869f1ad4e5294f6173a90d79de4765c5eed5
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-03-11 08:39:00 -0500

    first pass at getting functional tests to work

---
+++ examples/run_functional_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this script runs a few functional tests to make sure that everything
+# is working properly. on error in any subcommands, it should not quit
+# and finally exit with a non-zero exit code if any of the commands
+# failed
+exit_code=0
+
+# get the directory of this script
+# http://stackoverflow.com/q/242538/564709
+BASEDIR=$(dirname $0)
+
+# run the hello-world example and make sure it has the same results as
+# what is archived in our test archive
+cd $BASEDIR/hello-world
+workflow -fc
+exit_code=$(expr ${exit_code} + $?)
+workflow
+exit_code=$(expr ${exit_code} + $?)
+
+
+# exit with the sum of the status
+exit ${exit_code}
