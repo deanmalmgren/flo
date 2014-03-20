3332dbb273c929a122ecda170966e665f034c48b
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-03-20 09:20:53 -0500

    added numpy and matplotlib to requirements for travis-ci

--- provision/debian.sh
+++ provision/debian.sh
@@ -2,5 +2,5 @@
 
 # these are technically in the examples/REQUIREMENTS, too, but
 # installing dependencies is a bit of a pain in the ass so use this to
-# get all the dependencies first before rebuilding latest using pip
+# get all the dependencies first before rebuilding latest using pip.
 sudo apt-get install -y python-numpy python-matplotlib
