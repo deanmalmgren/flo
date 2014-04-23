cdfb9cf475c22c90876c78c39a687f0dc6ff28d5
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-04-23 15:27:53 -0500

    replicating rtd theme locally

--- docs/conf.py
+++ docs/conf.py
@@ -258,3 +258,11 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# on_rtd is whether we are on readthedocs.org
+# http://read-the-docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
