678a012d0cd018531d594ceb6369a1c3ab5616b8
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-04-28 09:24:38 -0500

    made sink/source tasks into lists for predictable ordering

--- flo/tasks/graph.py
+++ flo/tasks/graph.py
@@ -83,36 +83,34 @@ class TaskGraph(object):
             updownstream = 'upstream_tasks'
         horizon = collections.deque(tasks)
         done, horizon_set = set(), set(tasks)
-        task_order = []
         popmethod = getattr(horizon, popmethod)
         while horizon:
             task = popmethod()
             horizon_set.discard(task)
             done.add(task)
-            task_order.append(task)
+            yield task
             updownset = getattr(task, updownstream)
             for task in updownset.difference(done):
                 if task not in horizon_set:
                     horizon.append(task)
                     horizon_set.add(task)
-        return task_order
 
     def get_source_tasks(self):
         """Get the set of tasks that do not depend on anything else.
         """
-        source_tasks = set()
+        source_tasks = []
         for task in self.task_list:
             if not task.upstream_tasks:
-                source_tasks.add(task)
+                source_tasks.append(task)
         return source_tasks
 
     def get_sink_tasks(self):
         """Get the set of tasks that do not have any dependencies.
         """
-        sink_tasks = set()
+        sink_tasks = []
         for task in self.task_list:
             if not task.downstream_tasks:
-                sink_tasks.add(task)
+                sink_tasks.append(task)
         return sink_tasks
 
     def get_out_of_sync_tasks(self):
