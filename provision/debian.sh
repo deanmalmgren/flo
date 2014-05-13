63a854794c263e2acb1c56542bc6cf0e81c92ded
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-05-12 22:12:33 -0500

    fixed build

--- provision/debian.sh
+++ provision/debian.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# get the base directory name of this file
-# http://stackoverflow.com/a/11114547/564709
-sudo apt-get install -y realpath
-base=$(dirname $(realpath $0))/..
+# this needs to work both for vagrant provisioning and for travis
+# builds in a python virtualenv. in the virtual machine provisioning,
+# we're passing the directory this should be run from. in travis-ci,
+# its run from the root of the repository.
+if [ "$#" -eq 1 ]; then
+    cd $1
+    base=$(pwd)
+else
+    # get the base directory name of this file so it works in
+    # travis-ci environment http://stackoverflow.com/a/11114547/564709
+    sudo apt-get install -y realpath
+    base=$(dirname $(realpath $0))/..
+fi
 
 # install all of the dependencies required in the examples
-sed 's/\(.*\)\#.*/\1/' < $base/examples/DEBIAN | xargs sudo apt-get install -y
+sed 's/\(.*\)\#.*/\1/' < $base/examples/DEBIAN | xargs sudo apt-get install -y --fix-missing
